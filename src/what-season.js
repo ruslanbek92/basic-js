3bdd580c83649a8ead58527feca6d240031e5433
Ruslanbek Normurodov <aksirus89@gmail.com>
2023-04-17 14:54:23 +0500

    feat:add what season

--- src/what-season.js
+++ src/what-season.js
@@ -12,39 +12,45 @@ const { NotImplementedError } = require("../extensions/index.js");
  *
  */
 function getSeason(date) {
-//   let season;
-//   if (arguments.length === 0) {
-//   return 'Unable to determine the time of year!'
-//   }
-//   else if (!(date instanceof Date) ) {
-//     throw "Invalid date!";
-//   }
-//   else {
-//     switch (date.getMonth()) {
-//       case 2:
-//       case 3:
-//       case 4:
-//         season = "spring"
-//         break;
-//         case 5:
-//         case 6:
-//       case 7:
-//         season = "summer"
-//         break;
-//       case 8:
-//       case 9:
-//       case 10:
-//         season = "autumn"
-//         break;
-//       case 11:
-//       case 12:
-//       case 1:
-//         season = "winter";
-//         break;
+  if (date===undefined) {
+    return 'Unable to determine the time of year!'
+  }else if(!(date instanceof Date)){
+    throw new Error("Invalid date!");
+  } 
+  else {
+    console.log(date);
+let month = date.getMonth()
+console.log(month);
+let season ;
 
-//     }
-//     return season;
-//   }
+switch (month) {
+  case 2:
+  case 3:
+  case 4:
+   season= "spring";
+   break;
+  case 5:
+  case 6:
+  case 7:
+    season= "summer";
+    break;
+  case 8:
+    case 9:
+    case 10:
+      season= "autumn";
+      break;
+  case 11:
+  case 0:
+  case 1:
+    season= "winter";
+   break;  
+  default:
+    break;
+}
+
+return season;
+  }
+
  }
 
 module.exports = {
